68ba77e031abf356cd4a0afd9720f5548add19ba
Andrew Sykes <andrewsykes@btinternet.com>
2026-03-30 15:01:52 +0100

    connect4

--- api/index.php
+++ api/index.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/db.inc.php';
 $sql = "SELECT * from employee";
 $st = doQuery($pdo, $sql, 'fail');
 $rows = $st->fetchAll(PDO::FETCH_ASSOC); 
+
+dump($rows);
 include TEMPLATE . 'head.html.php'; ?>
 <h1>Jumpstart</h1>
 <ul>
